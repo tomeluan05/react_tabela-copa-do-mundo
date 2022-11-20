0f5093269213703d49a07c50d1ad79bb759ea061
Tomé Luan <tomeluan05@gmail.com>
2022-11-20 17:13:50 -0300

    criando o projeto da tabela da copa do mundo 2022

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h3>Iniciando o projeto tabela copa do mundo com REACT JS</h3>
+    </div>
+  )
+}
+
+export default App
